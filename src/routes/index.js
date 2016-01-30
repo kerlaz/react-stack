d1e6e062b7181b8355d4344a6cf0217d7765631b
Yerlan Bakuyev <yerlan.bakuyev@gmail.com>
2016-01-30 12:07:41 +0600

    1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import App from '../components/App.jsx';
+import Chat from '../components/Chat.jsx';
+import Login from '../components/Login.jsx';
+import Router from 'react-router';
 
+let Route = Router.Route;
+let DefaultRoute = Router.DefaultRoute;
